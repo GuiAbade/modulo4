e4bd73e4dc1367b445d35fa87802f67841e3e91c
Guilherme Abade <abade.guim13@gmail.com>
2025-02-16 17:32:01 -0300

    Usando funções

---
+++ app.py
@@ -0,0 +1,48 @@
+""" def nome_da_funcao(parametros):
+  comandos """
+
+
+""" def dar_boas_vindas():
+    print('Bem-vindo!')
+
+dar_boas_vindas()
+ """
+
+
+""" def dar_boas_vindas_personalizada(nome, idade, last):
+    print(f'Bem-vindo(a) {nome}')
+
+
+dar_boas_vindas_personalizada('Guilherme') """
+
+# Valor padrão
+
+""" 
+def apresentar_lugar(horario_de_funcionamento, lugar='Nossa loja'):
+    print(
+        f'Conheça {lugar}, horario de funcionamento das {horario_de_funcionamento}')
+
+
+apresentar_lugar('08:00 as 18:00', 'Disney') """
+
+
+# Desafio
+
+""" nome = input('Informe seu nome: ')
+sobrenome = input('Informe seu sobrenome: ')
+
+
+def mensagem(nome, sobrenome):
+    print(f'Seja bem-vindo {nome} {sobrenome}!')
+
+
+mensagem(nome, sobrenome) """
+
+# Desafio 2
+
+
+def calcula_valores(preco, qtd=1):
+    print(preco*qtd)
+
+
+calcula_valores(10, 2)
